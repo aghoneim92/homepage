7c461ff3574656ecbb0fa49f6805e0b2916dd3e1
Ahmed Ghoneim <ahmedghoneim92@gmail.com>
2022-04-22 10:32:20 +0200

    Add meta image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,8 @@ import Document, {
   Html,
   Main,
   NextScript,
-} from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default function DocumentComponent() {
   return (
@@ -21,6 +21,34 @@ export default function DocumentComponent() {
           href="https://fonts.googleapis.com/css2?family=Ubuntu&family=Red+Hat+Display&display=swap"
           rel="stylesheet"
         />
+        <meta name="title" content="Ahmed Ghoneim's Homepage" />
+        <meta name="description" content="Ahmed Ghoneim - Software Engineer" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://ahmedghoneim.site/" />
+        <meta property="og:title" content="Ahmed Ghoneim's Homepage" />
+        <meta
+          property="og:description"
+          content="Ahmed Ghoneim - Software Engineer"
+        />
+        <meta
+          property="og:image"
+          content="https://ahmedghoneim.site/meta-homepage.png"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="628" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://ahmedghoneim.site/" />
+        <meta property="twitter:title" content="Ahmed Ghoneim's Homepage" />
+        <meta
+          property="twitter:description"
+          content="Ahmed Ghoneim - Software Engineer"
+        />
+        <meta
+          property="twitter:image"
+          content="https://ahmedghoneim.site/meta-homepage.png"
+        />
+        <meta property="twitter:image:width" content="1200" />
+        <meta property="twitter:image:height" content="628" />
       </Head>
       <body>
         <Main />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import styled from "styled-components";
 import HomepageHeader from "../components/Homepage/HomepageHeader/HomepageHeader";
 import HomepageMain from "../components/Homepage/HomepageMain/HomepageMain";
 
