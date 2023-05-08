563dfdf25cb6a99dbea93eeffe9d545fca1cae95
Ahmed Ghoneim <ahmedghoneim92@gmail.com>
2023-05-08 21:02:05 +0300

    Add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Red_Hat_Display } from "next/font/google";
 import styles from "./layout.module.css";
 import Footer from "./Footer/Footer";
+import Head from "next/head";
 
 const redHatDisplay = Red_Hat_Display({ subsets: ["latin"] });
 
@@ -18,6 +19,36 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="title" content="Ahmed Ghoneim's Homepage" />
+        <meta name="description" content="Ahmed Ghoneim - Software Engineer" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://ahmedghoneim.site/" />
+        <meta property="og:title" content="Ahmed Ghoneim's Homepage" />
+        <meta
+          property="og:description"
+          content="Ahmed Ghoneim - Software Engineer"
+        />
+        <meta
+          property="og:image"
+          content="https://ahmedghoneim.site/meta-homepage.png"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="628" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://ahmedghoneim.site/" />
+        <meta property="twitter:title" content="Ahmed Ghoneim's Homepage" />
+        <meta
+          property="twitter:description"
+          content="Ahmed Ghoneim - Software Engineer"
+        />
+        <meta
+          property="twitter:image"
+          content="https://ahmedghoneim.site/meta-homepage.png"
+        />
+        <meta property="twitter:image:width" content="1200" />
+        <meta property="twitter:image:height" content="628" />
+      </Head>
       <body className={redHatDisplay.className}>
         <div className={styles.container}>
           <Navbar />
